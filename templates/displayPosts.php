bd154422afa463d67045def14e3e6f8e8b7de07f
asmith23 <adamJsmith80@outlook.com>
2023-06-01 19:48:41 +1000

    added user posts to only be edited by the user that made it.

--- templates/displayPosts.php
+++ templates/displayPosts.php
@@ -1,86 +1,68 @@
 
+
+
+
 <?php 
 
 include './includes/connect.inc.php';
 
-
-
-$sql = "SELECT * from posts;";
+$sql = "SELECT * FROM posts";
 $result = $conn->query($sql);
 
-
-if($result->num_rows > 0) {
-
+if ($result->num_rows > 0) {
   while ($row = $result->fetch_assoc()) {
-
-   
-
     $title = $row['title'];
     $review = $row['review'];
     $imageUrl = $row['imageUrl'];
-    $id = $row['id'];
+    $postId = $row['id'];
     $postUserId = $row['idUsers'];
 
-    
-
-// prepared statement to get the username associated with the posted review
+    // Prepared statement to get the username associated with the posted review
     $userSql = "SELECT uidUsers, idUsers FROM users WHERE idUsers = ?";
-
     $userStmt = $conn->prepare($userSql);
-    $userStmt->bind_param("i", $id);
+    $userStmt->bind_param("i", $postUserId);
     $userStmt->execute();
     $userResult = $userStmt->get_result();
 
-    if($userResult->num_rows > 0) {
+    if ($userResult->num_rows > 0) {
       $userRow = $userResult->fetch_assoc();
       $username = $userRow['uidUsers'];
-      $uid = $userRow['idUsers'];
+      $userId = $userRow['idUsers'];
     } else {
       $username = "Error, no username found";
     }
 
-    if (isset($_SESSION['userId'])) {
-      $postUserId = $row['idUsers'];
-      $loggedInUserId = $_SESSION['userId'];
-    }
     
 
-    // debug statements to check if the two values im trying to compare are correct
-  //  var_dump($postUserId);
-  //  var_dump($uid);
-    //card structure to display information from database
     echo '<div class="col-12 col-sm-12 col-xl-6">';
-        echo'<div class="card-container">';
-          echo '<div class="img-placeholder">';
-                echo'<img src="' . $imageUrl . '"alt="Image">';
-            echo '</div>';
-            echo '<div class="description">';
+    echo '<div class="card-container">';
+    echo '<div class="img-placeholder">';
+    echo '<img src="' . $imageUrl . '" alt="Image">';
+    echo '</div>';
+    echo '<div class="description">';
+    echo '<h4>' . $title . '</h4>';
+    echo '<div class="user-date-sect">';
+    echo '<p>posted by: ' . $username . '&nbsp; &nbsp; </p> || &nbsp; &nbsp; <p><em>Date: 01-01-1970</em></p>';
+    echo '</div>';
+    echo '<p class="user-review">' . $review . '</p>';
 
-              echo '<h4>' . $title . '</h4>';
-              echo ' <div class="user-date-sect">';
-                echo '<p>posted by: ' . $username .  '&nbsp; &nbsp; </p> || &nbsp; &nbsp; <p><em>Date: 01-01-1970</em></p>';
-                
-              echo '</div>';
-              echo '<p class="user-review">' . $review . '</p>';
-              // if ($postUserId) {
-                if ($uid === intval($postUserId)) {
-                echo '<div class="card-btn-container">';
-                echo '<button class="btn btn-primary m-1">EDIT</button>';
-                echo '<a href="includes/deletePost.inc.php?id=' . $id . '" class="btn btn-danger m-1">DELETE</a>';
-                echo '</div>';
-              // }
-
-              }
-              
-
-              
-
-            echo '</div>';
-          echo '</div>';
-        echo '</div>';
+    $postUserId = intval($postUserId);
+    $userId = intval($userId);
+    if (isset($_SESSION['userId']) && $_SESSION['userId'] == $userId) {
+      echo '<div class="card-btn-container">';
+      echo '<button class="btn btn-primary m-1">EDIT</button>';
+      echo '<a href="includes/deletePost.inc.php?id=' . $postId . '" class="btn btn-danger m-1">DELETE</a>';
+      echo '</div>';
     }
 
+    echo '</div>';
+    echo '</div>';
+    echo '</div>';
+  }
 } 
+    // var_dump($userId);
+    // var_dump($postUserId);
+    // var_dump($userRow['idUsers']);
 
 $conn->close();
 ?>
