4c12363aeed18142730a5f391d2ffce2fd657bb2
asmith23 <adamJsmith80@outlook.com>
2023-06-05 22:03:17 +1000

    save game

--- profile.php
+++ profile.php
@@ -1,8 +1,22 @@
 <?php require './templates/header.php' ?> 
 <?php include './templates/optionsBar.php' ?>
 
+<?php 
+require './includes/connect.inc.php';
+$userId = $_SESSION['userId'];
 
+$query = "SELECT * FROM profiles WHERE idUsers = ?";
+$stmt = $conn->prepare($query);
+$stmt->bind_param("i", $userId);
+$stmt->execute();
+$result = $stmt->get_result();
+$profile = $result->fetch_assoc();
+$stmt->close();
 
+$biography = $profile['biography'];
+$previousImageUrl = $profile['profileImageUrl'];
+
+?>
 
 <form action="./includes/editProfile.inc.php" method="POST" enctype="multipart/form-data">
   <div class="content-structure">
@@ -15,18 +29,30 @@
           <div class="form">
             <h3> Edit Profile </h3>
 
-            <div class="mb-3">
+            <?php if ($profile) { ?>
+              <div class="mb-3">
                 <label for="biography" class="form-label">Biography</label>
-                <textarea id="biography" type="textarea" class="form-control" name="biography" placeholder="Write something about yourself!" value=""></textarea>
+                <textarea id="biography" type="textarea" class="form-control" name="biography" placeholder="Write something about yourself!"><?php echo $biography; ?></textarea>
               </div>
 
-             
-
-              
               <div class="mb-3">
-                <label for="profileUrl" class="form-label">Select a profile picture: </label>
+                <label for="profileUrl" class="form-label">Select a profile picture:</label>
+                <input type="file" class="form-control" name="profileUrl">
+               
+              </div>
+              <!-- php else tag -->
+            <?php } else { ?>
+              <div class="mb-3">
+                <label for="biography" class="form-label">Biography</label>
+                <textarea id="biography" type="textarea" class="form-control" name="biography" placeholder="Write something about yourself!"></textarea>
+              </div>
+
+              <div class="mb-3">
+                <label for="profileUrl" class="form-label">Select a profile picture:</label>
                 <input type="file" class="form-control" name="profileUrl">
               </div>
+              <!-- php closing tag -->
+              <?php } ?>
 
               <button type="submit" name="profile-submit" class="btn btn-primary w-50">Submit profile</button>
 
--- templates/displayPosts.php
+++ templates/displayPosts.php
@@ -6,6 +6,8 @@
 
 include './includes/connect.inc.php';
 
+//Select all posts in "posts" table
+
 $sql = "SELECT * FROM posts";
 $result = $conn->query($sql);
 
@@ -25,6 +27,7 @@ if ($result->num_rows > 0) {
     $userStmt->execute();
     $userResult = $userStmt->get_result();
 
+    //check if the username exists
     if ($userResult->num_rows > 0) {
       $userRow = $userResult->fetch_assoc();
       $username = $userRow['uidUsers'];
@@ -33,44 +36,48 @@ if ($result->num_rows > 0) {
       $username = "Error, no username found";
     }
 
-    
-    
-
-    
-    
-    
-    
 
     echo '<div class="col-12 col-sm-12 col-xl-6">';
     echo '<div class="card-container">';
+    // container for the image, sitting on top of the description container
     echo '<div class="img-placeholder">';
     echo '<img src="' . $imageUrl . '" alt="Image">';
     echo '</div>';
+    //container that has two child containers, one sitting left ("title-review-sect") and one sitting right ("user-date-sect")
     echo '<div class="description">';
+    //section with title and review, this should sit left inside the "description" container
+    echo '<div class="title-review-sect">';
     echo '<h4>' . $title . '</h4>';
+    echo '<p class="user-review">' . $review . '</p>';
+    echo '</div>';
+    // section container with username and posted-date, on right side of "description" container
     echo '<div class="user-date-sect">';
     echo '<p>posted by: ' . $username . '</p>'; 
     echo '<p><em>' . date('F j, Y', strtotime($postCreatedAt)) . '</em></p>';
-
-
-    echo '</div>';
-
-    echo '<p class="user-review">' . $review . '</p>';
-
-    $postUserId = intval($postUserId);
-    $userId = intval($userId);
     if (isset($_SESSION['userId']) && $_SESSION['userId'] == $userId) {
+      $postUserId = intval($postUserId);
+      $userId = intval($userId);
       echo '<div class="card-btn-container">';
       echo '<a href="editPost.php?id=' . $postId . '" class="btn m-1"><i class="fa-solid fa-pen-to-square"></i></a>';
       echo '<a href="includes/deletePost.inc.php?id=' . $postId . '" class="btn m-1"><i class="fa-solid fa-trash"></i></a>';
       echo '</div>';
     }
+    echo '</div>';
+
+    
+
+    // if there is a user logged in, and that $_SESSION['userId'] matches the id of $postUserId (which is the id of the user that created the post), the delete and edit buttons will be visible on that post.
+
+    
+    
 
     echo '</div>';
     echo '</div>';
     echo '</div>';
   }
 } 
+
+// testing variable output at the end of this code block
     // var_dump($userId);
     // var_dump($postUserId);
     // var_dump($userRow['idUsers']);
